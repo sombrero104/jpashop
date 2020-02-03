f93b2c6af12412f2c233d8c0451c5b65044892ac
sombrero104 <sombrero.khy@gmail.com>
2020-02-03 20:27:03 +0900

    .

--- src/main/java/jpabook/jpashop/Member.java
+++ src/main/java/jpabook/jpashop/Member.java
@@ -2,16 +2,17 @@ package jpabook.jpashop;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Getter @Setter
 public class Member {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
     private String username;
 
