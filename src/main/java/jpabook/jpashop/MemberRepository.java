e14f4ee33f6e4e8a0e387a8e71dbda5854e314a0
sombrero104 <sombrero.khy@gmail.com>
2020-02-03 19:44:45 +0900

    .

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -16,6 +16,7 @@ public class MemberRepository {
     private EntityManager em;
 
     /**
+     * 회원 저장.
      * Member 객체를 반환하지 않고 id만 반환하는 이유.
      * => 커맨드와 쿼리를 분리하라.
      * 저장하는 목적의 사이드이펙트를 일으키는 커맨드성 메소드이기 때문에 리턴값이 거의 필요없음.
@@ -26,6 +27,9 @@ public class MemberRepository {
         return member.getId();
     }
 
+    /**
+     * 회원 id로 조회.
+     */
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
