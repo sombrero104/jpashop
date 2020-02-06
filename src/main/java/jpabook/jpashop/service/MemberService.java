f004f757534a900c7fada9b7db79a771d39047f3
sombrero104 <sombrero.khy@gmail.com>
2020-02-06 23:34:51 +0900

    .

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
 
     /**
@@ -37,13 +39,23 @@ public class MemberService {
      * 장점: 어플리케이션이 뜨고 서비스가 생성될 때에만 조립이 되기 때문에 중간에 set해서 바꿀 일이 없다.
      *  테스트 코드에서 MemberService 생성 시 MemberService가 MemberRepository를 의존하고 있다는 것을 명확하게 알 수 있다.
      *  스프링 최신 버전에서는 생성자가 하나만 있는 경우, @Autowired가 명시되어 있지 않아도 자동으로 injection 해준다.
+     *
+     * final을 명시해주면 생성자에서 값을 넣지 않는 경우 컴파일 에러가 나기 때문에 실수로 값을 넣지 않는 경우가 발생하지 않게 된다.
      */
-    private MemberRepository memberRepository;
+    /*private final MemberRepository memberRepository;
 
     @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
-    }
+    }*/
+
+    /**
+     * (4) 생성자 injection 방식에서 롬복 사용
+     * 롬복의 @AllArgsConstructor를 클래스에 명시해주면 필드를 가지고 위와 같이 값을 주입받는 생성자를 자동으로 만들어준다.
+     * 더 좋은 방법은 @RequiredArgsConstructor인데, final이 있는 필드만 가지고 위와 같이 값을 주입받는 생성자를 자동으로 만들어준다.
+     */
+    private final MemberRepository memberRepository;
+
 
 
 
