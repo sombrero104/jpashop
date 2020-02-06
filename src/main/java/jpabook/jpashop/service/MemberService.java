e2caff10ae3fb448f54a67dd2214d731b800689b
sombrero104 <sombrero.khy@gmail.com>
2020-02-06 23:28:51 +0900

    .

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -12,8 +12,41 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class MemberService {
 
+    /**
+     * [injection 방식]
+     *
+     * (1) 필드 injection 방식
+     */
+    /*@Autowired
+    private MemberRepository memberRepository;*/
+
+    /**
+     * (2) setter injection 방식
+     * 장점: 테스트 코드 작성 시 Mock을 직접 주입해서 사용할 수 있다.
+     * 단점: 실제 서비스 시에는 어플리케이션이 뜰 때 모든게 조립이 완성되므로 사용할 일이 없다. 잘못 사용하면 문제 발생.
+     */
+    /*private MemberRepository memberRepository;
+
     @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }*/
+
+    /**
+     * (3) 생성자 injection 방식
+     * 장점: 어플리케이션이 뜨고 서비스가 생성될 때에만 조립이 되기 때문에 중간에 set해서 바꿀 일이 없다.
+     *  테스트 코드에서 MemberService 생성 시 MemberService가 MemberRepository를 의존하고 있다는 것을 명확하게 알 수 있다.
+     *  스프링 최신 버전에서는 생성자가 하나만 있는 경우, @Autowired가 명시되어 있지 않아도 자동으로 injection 해준다.
+     */
     private MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+
 
     /**
      * 회원 가입
@@ -25,6 +58,10 @@ public class MemberService {
         return member.getId();
     }
 
+    /**
+     * WAS가 여러대인 경우 동시에 저장하게 되면 아래 validate를 동시에 통과하는 문제가 발생할 수도 있다.
+     * 때문에 DB에서 제약조건을 유니크로 걸어서 한번 더 방어하는 것이 좋다.
+     */
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
         if(!findMembers.isEmpty()) {
